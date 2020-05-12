f9ea85c425391737eeb2936e45437be01332e6c6
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-05-12 16:58:47 +0300

    ASP Net Core WCF EF Razor Pages

---
+++ PostComment/ASP.Net_Core_Razor/Models/PostDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_Core_Razor.Models
+{
+    public class PostDTO
+    {
+        public int PostId { get; set; }
+
+        public string Description { get; set; }
+
+        public string Domain { get; set; }
+
+        public System.DateTime Date { get; set; }
+        public virtual ICollection<CommentDTO> Comments { get; set; }
+
+        public PostDTO()
+        {
+            Comments = new List<CommentDTO>();
+        }
+    }
+}
